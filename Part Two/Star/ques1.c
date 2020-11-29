ee5fd0882a55b153c574ca0ea1151a7fa3475f29
adygregStudent <74949890+adygregStudent@users.noreply.github.com>
2020-11-29 16:55:02 +0530

    Add files via upload

---
+++ Part Two/Star/ques1.c
@@ -0,0 +1,37 @@
+/*
+                *
+              * * *
+            * * * * *
+          * * * * * * *
+        * * * * * * * * *
+       Pyramid Star Pattern
+
+    source by com.adygreg
+    [29:11:2020]
+
+*/
+
+#include <stdio.h>
+
+void pyPattern (int n) {
+
+    for (int i=1; i<=n; i++) {
+
+        for (int k=n; k>i; k--)
+            printf("  ");
+
+        for (int j=0; j<i*2-1; j++)
+            printf("* ");
+        
+        printf("\n");
+
+    }
+
+}
+
+int main (void) {
+
+    pyPattern (5);
+
+    return 0;
+}
---
+++ Part Two/Star/ques3.c
@@ -0,0 +1,37 @@
+/*
+        * * * * * * * * *
+          * * * * * * *
+            * * * * *
+              * * *
+                *
+    Inverted Pyramid Star Pattern
+
+    source by com.adygreg
+    [29:11:2020]
+
+*/
+
+#include <stdio.h>
+
+void pyPattern (int n) {
+
+    for (int i=n; i>=1; i--) {
+
+        for (int k=n; k>i; k--)
+            printf("  ");
+        
+        for (int j=i*2-1; j>0; j--)
+            printf("* ");
+        
+        printf("\n");
+
+    }
+
+}
+
+int main (void) {
+
+    pyPattern (5);
+
+    return 0;
+}
