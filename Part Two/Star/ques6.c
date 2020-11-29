c4795d317fbb3653141043dfbd1bf9f469781d50
adygregStudent <74949890+adygregStudent@users.noreply.github.com>
2020-11-29 16:56:10 +0530

    Add files via upload

---
+++ Part Two/Star/ques6.c
@@ -0,0 +1,55 @@
+/*
+               *
+              * *
+             * * *
+            * * * *
+           * * * * *
+            * * * *
+             * * *
+              * *
+               *
+ Mirrored Half Diamond Star Pattern
+
+    source by com.adygreg
+    [29:11:2020]
+
+*/
+
+#include <stdio.h>
+
+void diPattern (int n) {
+
+    for (int i=1; i<=n; i++) {
+
+        for (int k=n; k>i; k--)
+            printf(" ");
+
+        for (int j=0; j<i; j++)
+            printf("* ");
+        
+        printf("\n");
+
+    }
+
+    n--;
+
+    for (int i=0; i<=n-1; i++) {
+
+        for (int k=0; k<=i; k++)
+            printf(" ");
+
+        for (int j=n; j>i; j--)
+            printf("* ");
+        
+        printf("\n");
+
+    }
+
+}
+
+int main (void) {
+
+    diPattern (5);
+
+    return 0;
+}
